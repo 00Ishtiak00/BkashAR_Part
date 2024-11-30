aaea46e5394fab74166df1d17eda4157e4655457
Ishtiak Arafat <ishtiakarafat@gmail.com>
2024-12-01 02:48:53 +0600

    Client Shared

--- Assets/_Projects/Scripts/LockRotaion.cs
+++ Assets/_Projects/Scripts/LockRotaion.cs
@@ -6,16 +6,16 @@ public class LockRotation : MonoBehaviour
 
     void Start()
     {
-        // Store the initial rotation of the GameObject
-        initialRotation = transform.rotation;
+        // Store the initial local rotation of the GameObject
+        initialRotation = transform.localRotation;
     }
 
     void Update()
     {
-        // Lock X and Z rotation, but allow Y rotation
-        transform.rotation = Quaternion.Euler(
+        // Lock X and Z local rotation, but allow Y local rotation
+        transform.localRotation = Quaternion.Euler(
             initialRotation.eulerAngles.x, // Lock X axis
-            transform.rotation.eulerAngles.y, // Allow Y axis
+            transform.localRotation.eulerAngles.y, // Allow Y axis
             initialRotation.eulerAngles.z  // Lock Z axis
         );
     }
